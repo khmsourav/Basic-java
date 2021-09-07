143a115a6b33013a625338aacc0ac659363af746
khmsourav <32020708+khmsourav@users.noreply.github.com>
2021-09-08 00:03:12 +0600

    string
    string practices

---
+++ basic_00.java
@@ -0,0 +1,9 @@
+public class basic_00{
+public static void main(String args[]){
+	int a = +12002431;
+	System.out.println("My name is Kazi Sourav");
+	System.out.println("My phone number is " +a);
+	
+	
+}
+}
